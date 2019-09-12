94284a94fbfdd71f8fd80dac4967efa07c082a27
Ben Milman <bpmilman@gmail.com>
2019-09-11 19:44:14 -0700

    Switch from tslint to eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard',
+    '@vue/typescript',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi': 'off',
+    '@typescript-eslint/semi': ['warn'],
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+};
