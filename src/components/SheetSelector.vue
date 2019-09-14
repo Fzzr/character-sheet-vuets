b1793c5af77021fe46011d41a060a2d9f9e00965
Ben Milman <bpmilman@gmail.com>
2019-09-13 19:43:10 -0700

    Add sheet selection dropdown

---
+++ src/components/SheetSelector.vue
@@ -0,0 +1,34 @@
+<template>
+  <select
+    :value="selectedSheetId"
+    @change="updateSelected"
+  >
+    <option
+      v-for="sheet in sheets"
+      :key="sheet.id"
+      v-bind:value="sheet.id"
+    >
+      {{sheet.name}}
+    </option>
+  </select>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
+
+import { RootState } from '../types';
+
+@Component({
+  name: 'SheetSelector',
+})
+export default class SheetSelector extends Vue {
+  @Prop() private selectedSheetId!: string;
+  @Prop() private sheets!: Extract<RootState, 'sheets'>;
+
+  @Emit()
+  private updateSelected (event: Event) {
+    const target = event.target as HTMLSelectElement;
+    return target.value;
+  }
+}
+</script>
