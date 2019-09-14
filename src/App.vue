786dc896d8d2deeae9ef1f9c500b4de8f003ce7d
Ben Milman <bpmilman@gmail.com>
2019-09-13 19:43:41 -0700

    Add App functionality

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,76 @@
 <template>
   <div id="app">
+    <SheetSelector
+      @update-selected="changeSheet"
+      v-bind:selectedSheetId=selectedSheetId
+      v-bind:sheets=sheets
+    />
+    <SingleSheet
+      v-if="!isTabbedSheet()"
+      v-bind:sheet=selectedSheet
+    />
+    <TabbedSheet
+      v-if="isTabbedSheet()"
+      v-bind:sheet=selectedSheet
+    />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import {
+  mapGetters,
+  mapState,
+  mapMutations,
+} from 'vuex';
+import {
+  Component,
+  Prop,
+  Vue,
+} from 'vue-property-decorator';
+
+import SheetSelector from './components/SheetSelector.vue';
+import SingleSheet from './components/SingleSheet.vue';
+import TabbedSheet from './components/TabbedSheet.vue';
+
+import {
+  RootState,
+  SingleSheet as SingleSheetShape,
+  TabbedSheet as TabbedSheetShape,
+} from './types';
 
 @Component({
   components: {
+    SheetSelector,
+    SingleSheet,
+    TabbedSheet,
+  },
+  computed: {
+    ...mapState([
+      'selectedSheetId',
+      'sheets',
+    ]),
+    ...mapGetters([
+      'selectedSheet',
+    ]),
+  },
+  methods: {
+    ...mapMutations([
+      'changeSheet',
+    ]),
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  private selectedSheetId!: string;
+  private sheets!: Extract<RootState, 'sheets'>;
+  private selectedSheet!: SingleSheetShape | TabbedSheetShape;
+  private isTabbedSheet () {
+    if ((this.selectedSheet as TabbedSheetShape).tabs) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 </script>
 
 <style>
